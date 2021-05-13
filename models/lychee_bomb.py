47ef3b5f5d0557ef6ba20368277729d51472e3ea
JustTheInstinct <jordangmain@gmail.com>
2021-05-12 21:13:29 -0700

    fixes to lychee spikes

--- models/lychee_bomb.py
+++ models/lychee_bomb.py
@@ -29,17 +29,19 @@ class LycheeBomb(Plant):
             if self.frame_timer == 30:
                 # Spawn explosion and spikes
                 explosion = Explode(self, self.MainGame, self.MainView)
-                spike = LycheeSpike(self, self.MainGame, self.MainView)
+                if self.rect.y > 80:
+                    spike = LycheeSpike(self, self.MainGame, self.MainView)
+                    self.MainGame.lycheespike_list.append(spike)
                 self.rect.x = self.rect.x + 80
                 self.rect.y = self.rect.y + 80
                 spike_two = LycheeSpike(self, self.MainGame, self.MainView)
-                self.rect.x = self.rect.x - 80
-                self.rect.y = self.rect.y + 80
-                spike_three = LycheeSpike(self, self.MainGame, self.MainView)
-                self.MainGame.lycheespike_list.append(spike)
-                self.MainGame.explosion_list.append(explosion)
                 self.MainGame.lycheespike_list.append(spike_two)
-                self.MainGame.lycheespike_list.append(spike_three)
+                if self.rect.y < 480:
+                    self.rect.x = self.rect.x - 80
+                    self.rect.y = self.rect.y + 80
+                    spike_three = LycheeSpike(self, self.MainGame, self.MainView)
+                    self.MainGame.lycheespike_list.append(spike_three)
+                self.MainGame.explosion_list.append(explosion)
                 # Reset planting ability
                 a = self.rect.y // 80 - 1
                 b = self.rect.x // 80
