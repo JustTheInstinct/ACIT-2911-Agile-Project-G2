8a7d3afe5a0ee2e369e66cd2831bd9caa3cd970e
Di Gao <d986000777@gmail.com>
2021-05-14 00:46:28 -0700

    Remove useless comment

--- controllers/base.py
+++ controllers/base.py
@@ -8,13 +8,10 @@ class PygameController:
         while running:
             clock.tick(30)
             for event in pygame.event.get():
-                # Invoked close from the OS
                 if event.type == pygame.locals.QUIT:
                     return False
-                # Pressed a key - is it Escape?
                 elif event.type == pygame.locals.KEYDOWN:
                     if event.key == pygame.locals.K_ESCAPE:
                         return False
-                # Clicked the mouse
                 elif event.type == pygame.locals.MOUSEBUTTONDOWN:
                     return event.pos
