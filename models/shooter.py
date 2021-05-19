b8dd4941b4fd673b36603d81e0f714b8f3db7a39
Di Gao <d986000777@gmail.com>
2021-05-18 17:13:59 -0700

    Rewrite some models to make them testable

--- models/shooter.py
+++ models/shooter.py
@@ -14,20 +14,21 @@ class PeaShooter(Plant):
         self.price = 50
         self.hp = 200
         self.shot_count = 0
-        self.should_fire = False
-
-        # self.shoot_sound = mixer.Sound("./sounds/shoop1.wav")
-        # pygame.mixer.Sound.set_volume(self.shoot_sound, 0.3)
-
+    
+    def shootsound(self):
+        self.shoot_sound = mixer.Sound("./sounds/shoop1.wav")
+        pygame.mixer.Sound.set_volume(self.shoot_sound, 0.3)
+        self.shoot_sound.play()
 
     def shot(self):
+        should_fire = False
         for zombie in self.MainGame.zombie_list:
             if zombie.rect.y == self.rect.y and zombie.rect.x < 1050 and zombie.rect.x > self.rect.x:
-                self.should_fire = True
-        if self.live and self.should_fire:
+                should_fire = True
+        if self.live and should_fire:
             self.shot_count += 1
             if self.shot_count == 25:
-                self.shoot_sound.play()
+                self.shootsound()
                 peabullet = PeaBullet(self, self.MainGame, self.MainView)
                 self.MainGame.peabullet_list.append(peabullet)
                 self.shot_count = 0
