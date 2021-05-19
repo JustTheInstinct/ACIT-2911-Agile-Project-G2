c26918636e13c5f0add68acd9deb62ecee09c538
Tamatekk <rpi.irman@gmail.com>
2021-05-19 08:05:53 -0700

    snow tests

--- tests/test_snowpea.py
+++ tests/test_snowpea.py
@@ -10,6 +10,8 @@ class maing():
         self.GAMEOVER  = False
 mg = maing(0)
 
+zombie = Norzombie(200,100, mg, 'MainView')
+
 def test_snow_att():
     new_snow = SnowPea(5,1,mg,'mainview')
     assert new_snow.hp == 200
@@ -18,6 +20,11 @@ def test_snow_att():
     assert new_snow.rect.x == 5
     assert new_snow.rect.y == 1
 
-def test_shoot():
+def test_shot():
     new_snow = SnowPea(5,1,mg,'mainview')
-    new_zombie = Norzombie(5,1,mg,'mainview')
+    if zombie == Norzombie(10,1, mg, 'MainView'):
+        assert new_snow.should_fire == True
+    if zombie == Norzombie(10,5, mg, 'MainView'):
+        assert new_snow.should_fire == False
+    if zombie == Norzombie(1,1, mg, 'MainView'):
+        assert new_snow.should_fire == False
