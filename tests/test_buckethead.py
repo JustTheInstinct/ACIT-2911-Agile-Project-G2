9fe3c662df3e626d155b235495d60b05ab140362
Tamatekk <rpi.irman@gmail.com>
2021-05-18 17:45:23 -0700

    separated unit test into files

---
+++ tests/test_buckethead.py
@@ -0,0 +1,24 @@
+import pytest
+from models import Buckethead
+
+class maing():
+    def __init__(self, money):
+        self.money = money
+        self.hp = 100
+        self.score = 0
+        self.remnant_score = 40
+mg = maing(0)
+
+def test_bucket_att():
+    new_buckethead = Buckethead(5,1,mg,'mainview')
+    assert new_buckethead.live == True and new_buckethead.hp == 1000
+
+def test_eat():
+    new_buckethead = Buckethead(5,1,mg,'mainview')
+    new_buckethead.eat_plant(mg)
+    assert new_buckethead.stop == False
+
+def test_move():
+    new_buckethead = Buckethead(1,1,mg,'mainview')
+    new_buckethead.move_zombie()
+    assert new_buckethead.rect.x == 0
