0cfca0d428fe7377daa779ca63a78f20a58172df
Di Gao <d986000777@gmail.com>
2021-05-18 17:24:30 -0700

    fix errors in peashooter

--- models/shooter.py
+++ models/shooter.py
@@ -14,18 +14,18 @@ class PeaShooter(Plant):
         self.price = 50
         self.hp = 200
         self.shot_count = 0
-    
+        self.should_fire = False
+
     def shootsound(self):
         self.shoot_sound = mixer.Sound("./sounds/shoop1.wav")
         pygame.mixer.Sound.set_volume(self.shoot_sound, 0.3)
         self.shoot_sound.play()
 
     def shot(self):
-        should_fire = False
         for zombie in self.MainGame.zombie_list:
             if zombie.rect.y == self.rect.y and zombie.rect.x < 1050 and zombie.rect.x > self.rect.x:
-                should_fire = True
-        if self.live and should_fire:
+                self.should_fire = True
+        if self.live and self.should_fire:
             self.shot_count += 1
             if self.shot_count == 25:
                 self.shootsound()
